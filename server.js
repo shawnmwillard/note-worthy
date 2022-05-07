975f6ec79cd1703bbde3885ba5d2a6e7c1e9b0aa
Shawn Willard <shawnmwillard@gmail.com>
2022-05-07 16:07:27 -0500

    comments added to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Require Express
 const express = require("express");
 const path = require("path");
 
@@ -6,6 +7,8 @@ const app = express();
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+//Run on PORT 3001
 const PORT = process.env.PORT || 3001;
 
+//Let me know when / where server is started successfully
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
