e759aa55f63777474992d7351ec4405531ba7e0c
Shawn Willard <shawnmwillard@gmail.com>
2022-05-07 16:02:55 -0500

    static server setup

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3001;
 
