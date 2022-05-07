52b24eade05ced1eda80f7b9e23e415fd9b03e0e
Shawn Willard <shawnmwillard@gmail.com>
2022-05-07 17:12:37 -0500

    commit for deploy to heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Develop", "public")));
 
 //Run on PORT 3001
 const PORT = process.env.PORT || 3001;
